487d7c36bb87d45fb7f98f5bd6c7dd0bed9b8c08
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2025-03-09 20:54:41 +0100

    Add Poafloc library

---
+++ ports/poafloc/portfile.cmake
@@ -0,0 +1,29 @@
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL git://git.dimitrijedobrota.com/poafloc.git
+    REF 5c4cf9d0404e3ccdfd69206e6d9faeef1203666a
+    HEAD_REF master
+)
+
+set(name poafloc)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        "-D${name}_INSTALL_CMAKEDIR=share/${name}"
+    OPTIONS_DEBUG
+        "-D${name}_INSTALL_HEADERS=OFF"
+)
+
+# set(VCPKG_BUILD_TYPE release) # header-only
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME "${name}")
+vcpkg_copy_pdbs()
+
+
+configure_file(
+    "${SOURCE_PATH}/LICENSE.md"
+    "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright"
+    COPYONLY
+)
