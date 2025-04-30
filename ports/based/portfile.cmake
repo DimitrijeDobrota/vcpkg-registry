cc7113e63d7bb988a70e6b6dd91c5cd1af04bdfb
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2025-04-30 11:43:21 +0200

    Add Based library

---
+++ ports/based/portfile.cmake
@@ -0,0 +1,29 @@
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL git://git.dimitrijedobrota.com/based.git
+    REF 523b08ac6fe91c3418e5613ccaee506a7c4aa781
+    HEAD_REF master
+)
+
+set(name based)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        "-D${name}_INSTALL_CMAKEDIR=share/${name}"
+    OPTIONS_DEBUG
+        "-D${name}_INSTALL_HEADERS=OFF"
+)
+
+# set(VCPKG_BUILD_TYPE release) # header-only
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME "${name}")
+vcpkg_copy_pdbs()
+
+
+configure_file(
+    "${SOURCE_PATH}/LICENSE.md"
+    "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright"
+    COPYONLY
+)
