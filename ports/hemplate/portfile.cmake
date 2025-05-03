b43dfee24a1d81438e1ad87acfdb6f729c72f73b
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2025-05-03 14:23:22 +0200

    Add Hemplate library

---
+++ ports/hemplate/portfile.cmake
@@ -0,0 +1,29 @@
+vcpkg_from_git(
+    OUT_SOURCE_PATH SOURCE_PATH
+    URL git://git.dimitrijedobrota.com/hemplate.git
+    REF 71dee1ad15475c166cb7e23e7d86e93428ed0ec1
+    HEAD_REF master
+)
+
+set(name hemplate)
+
+vcpkg_cmake_configure(
+    SOURCE_PATH "${SOURCE_PATH}"
+    OPTIONS
+        "-D${name}_INSTALL_CMAKEDIR=share/${name}"
+    OPTIONS_DEBUG
+        "-D${name}_INSTALL_HEADERS=OFF"
+)
+
+# set(VCPKG_BUILD_TYPE release) # header-only
+
+vcpkg_cmake_install()
+vcpkg_cmake_config_fixup(PACKAGE_NAME "${name}")
+vcpkg_copy_pdbs()
+
+
+configure_file(
+    "${SOURCE_PATH}/LICENSE.md"
+    "${CURRENT_PACKAGES_DIR}/share/${PORT}/copyright"
+    COPYONLY
+)
