4fbc09f4557aef544dd9afd9fee9f80c9db1e819
Dimitrije Dobrota <mail@dimitrijedobrota.com>
2025-06-07 18:52:36 +0200

    Based version 0.2

--- ports/based/portfile.cmake
+++ ports/based/portfile.cmake
@@ -1,7 +1,7 @@
 vcpkg_from_git(
     OUT_SOURCE_PATH SOURCE_PATH
     URL git://git.dimitrijedobrota.com/based.git
-    REF e23e7e98862df4b47ff361e3151c685104e45ca1
+    REF 9c27b6097475aed034e0459915881abb1f6fd2e0
     HEAD_REF master
 )
 
@@ -11,11 +11,9 @@ vcpkg_cmake_configure(
     SOURCE_PATH "${SOURCE_PATH}"
     OPTIONS
         "-D${name}_INSTALL_CMAKEDIR=share/${name}"
-    OPTIONS_DEBUG
-        "-D${name}_INSTALL_HEADERS=OFF"
 )
 
-# set(VCPKG_BUILD_TYPE release) # header-only
+set(VCPKG_BUILD_TYPE release) # header-only
 
 vcpkg_cmake_install()
 vcpkg_cmake_config_fixup(PACKAGE_NAME "${name}")
